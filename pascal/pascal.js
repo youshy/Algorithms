5dac031d722904f035f20ae7bb8b9c8ec6d30586
Artur <devilyoushy@gmail.com>
2018-11-05 22:06:31 +0000

    v.2.0 added main hub, added functionality for Pascal's triangle, probably will work on Fibonacci's

--- pascal/pascal.js
+++ pascal/pascal.js
@@ -2,10 +2,8 @@
 
 const pascalTriangle = (rowsNum) => {
     let pascalTriangle = [];
-
     for (let i=0; i<rowsNum; i++) {
         pascalTriangle[i] = new Array(i+1);
-
         for (let j=0; j<i+1; j++) {
             if (j === 0 || j === i) {
                 pascalTriangle[i][j] = 1;
@@ -14,29 +12,47 @@ const pascalTriangle = (rowsNum) => {
             }
         }
     }
-
     return pascalTriangle;
 }
+
+// If I want to get the result in the table in the console
 
 const drawTable = (number) => {
     console.table(pascalTriangle(number))
 }
 
-// Draw on screen
+// Fetch the value from input
 
-const drawer = (number) => {
-    const result = pascalTriangle(number);
+const fetcher = () => {
+    const numberBox = document.getElementById('numberBox');
+    let numberBoxValue = numberBox.value;
+    return numberBoxValue;
+}
+
+// Erase from the screen
+
+const eraser = () => {
+    const draw = document.getElementById('draw');
+    let childNode = document.getElementById('result');
+    if (childNode !== null) {
+        draw.removeChild(childNode);
+    }
+}
+
+// Draw on the screen
+
+const drawer = () => {
+    eraser();
     const draw = document.getElementById("draw");
-    const container = document.createElement("div");
+    const result = pascalTriangle(fetcher());
+    const container = document.createElement('div');
+    container.id = 'result';
     draw.appendChild(container);
     for (let i=0; i<result.length; i++) {
-        console.log(`line ${i}`);
-        console.log(`${result[i]}`);
-        console.log(result[i].length);
-        let pTag = document.createElement("p");
+        let pTag = document.createElement('p');
         container.appendChild(pTag);
         pTag.id = `line${i}`;
-        let text = `${result[i]}`;
+        let text = `${result[i]}`.toString().split(",").join(' ');
         let textValue = document.createTextNode(text);
         pTag.appendChild(textValue);
     }
