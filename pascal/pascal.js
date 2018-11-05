67df10ce70af7895de92c5ef0409e424d0d0f457
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2018-11-05 15:33:03 +0000

    v.1.1 now you can render that on the page

--- pascal/pascal.js
+++ pascal/pascal.js
@@ -1,3 +1,5 @@
+// Functions
+
 const pascalTriangle = (rowsNum) => {
     let pascalTriangle = [];
 
@@ -20,3 +22,22 @@ const drawTable = (number) => {
     console.table(pascalTriangle(number))
 }
 
+// Draw on screen
+
+const drawer = (number) => {
+    const result = pascalTriangle(number);
+    const draw = document.getElementById("draw");
+    const container = document.createElement("div");
+    draw.appendChild(container);
+    for (let i=0; i<result.length; i++) {
+        console.log(`line ${i}`);
+        console.log(`${result[i]}`);
+        console.log(result[i].length);
+        let pTag = document.createElement("p");
+        container.appendChild(pTag);
+        pTag.id = `line${i}`;
+        let text = `${result[i]}`;
+        let textValue = document.createTextNode(text);
+        pTag.appendChild(textValue);
+    }
+}
