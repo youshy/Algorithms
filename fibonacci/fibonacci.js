4dad208a3c2e3799dd1cf886ebce5245c1b03863
Artur <devilyoushy@gmail.com>
2018-11-08 18:12:18 +0000

    v.4.1 fixed indentation

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,12 +1,14 @@
 // Variables
 
 const draw = document.getElementById("draw");
-const numberBox = document.getElementById('numberBox');
-const controls = document.getElementById('controlBox');
-const firstImplementation = document.getElementById('firstImplementation');
-const firstImplementationArray = document.getElementById('firstImplementationArray');
-const secondImplementation = document.getElementById('secondImplementation');
-const thirdImplementation = document.getElementById('thirdImplementation');
+const numberBox = document.getElementById("numberBox");
+const controls = document.getElementById("controlBox");
+const firstImplementation = document.getElementById("firstImplementation");
+const firstImplementationArray = document.getElementById(
+  "firstImplementationArray"
+);
+const secondImplementation = document.getElementById("secondImplementation");
+const thirdImplementation = document.getElementById("thirdImplementation");
 let fibonacciFirstArray = [];
 
 // Clicker checker and also functions
@@ -16,145 +18,151 @@ let secondImplementationClicked = false;
 let thirdImplementationClicked = false;
 
 const clickHandlerForFirst = () => {
-    firstImplementationClicked = true;
-}
+  firstImplementationClicked = true;
+};
 
 const clickHandlerForSecond = () => {
-    secondImplementationClicked = true;
-}
+  secondImplementationClicked = true;
+};
 
 const clickHandlerForThird = () => {
-    thirdImplementationClicked = true;
-}
+  thirdImplementationClicked = true;
+};
 
 // First implementation
 
-const fibonacciFirst = (num) => {
-    let a = 1, b = 0, temp;
-    fibonacciFirstArray = [];
-    while (num >= 0) {
-        temp = a;
-        a = a + b;
-        b = temp;
-        num--;
-        fibonacciFirstArray.push(temp);
-    }
-    return b;
-}
+const fibonacciFirst = num => {
+  let a = 1,
+    b = 0,
+    temp;
+  fibonacciFirstArray = [];
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+    fibonacciFirstArray.push(temp);
+  }
+  return b;
+};
 
 // Second implementation - recursive - shows the element with given index
 
-const fibonacciSecond = (num) => {
-    if (num <= 1) return 1;
-    let result = fibonacciSecond(num - 1) + fibonacciSecond(num - 2);
-    return result;
-}
+const fibonacciSecond = num => {
+  if (num <= 1) return 1;
+  let result = fibonacciSecond(num - 1) + fibonacciSecond(num - 2);
+  return result;
+};
 
 // Third implementation - using memoization
 
 const fibonacciThird = (num, memo) => {
-    memo = memo || {};
-    if (memo[num]) return memo[num];
-    if (num <= 1) return 1;
-    return memo[num] = fibonacciThird(num - 1, memo) + fibonacciThird(num - 2, memo);
-}
+  memo = memo || {};
+  if (memo[num]) return memo[num];
+  if (num <= 1) return 1;
+  return (memo[num] =
+    fibonacciThird(num - 1, memo) + fibonacciThird(num - 2, memo));
+};
 
 // Fetch the value from input
 
 const fetcher = () => {
-    let numberBoxValue = numberBox.value;
-    return numberBoxValue - 1;
-}
+  let numberBoxValue = numberBox.value;
+  return numberBoxValue - 1;
+};
 
 // Erase from the screen
 
 const eraser = () => {
-    while (draw.firstChild) {
-        draw.removeChild(draw.firstChild);
-    }
-}
+  while (draw.firstChild) {
+    draw.removeChild(draw.firstChild);
+  }
+};
 
 // Checks which button has been clicked
 
 const letMeCheck = () => {
-    if (firstImplementationClicked === true) {
-        let returnValue = fibonacciFirst(fetcher());
-        firstImplementationClicked = false;
-        return returnValue;
-    } else if (secondImplementationClicked === true) {
-        let returnValue = fibonacciSecond(fetcher());
-        secondImplementationClicked = false;
-        return returnValue;
-    } else if (thirdImplementationClicked === true) {
-        let returnValue = fibonacciThird(fetcher());
-        thirdImplementationClicked = false;
-        return returnValue;
-    } else {
-        console.log(`well, something went bad...`)
-    }
-}
+  if (firstImplementationClicked === true) {
+    let returnValue = fibonacciFirst(fetcher());
+    firstImplementationClicked = false;
+    return returnValue;
+  } else if (secondImplementationClicked === true) {
+    let returnValue = fibonacciSecond(fetcher());
+    secondImplementationClicked = false;
+    return returnValue;
+  } else if (thirdImplementationClicked === true) {
+    let returnValue = fibonacciThird(fetcher());
+    thirdImplementationClicked = false;
+    return returnValue;
+  } else {
+    console.log(`well, something went bad...`);
+  }
+};
 
 // Append element on the screen
 
-const elementDrawer = (whatToDraw) => {
-    const container = document.createElement('div');
-    container.id = 'result';
-    draw.appendChild(container);
-    const pTag = document.createElement('p');
-    container.appendChild(pTag);
-    const textValue = document.createTextNode(whatToDraw);
-    pTag.appendChild(textValue);
-}
+const elementDrawer = whatToDraw => {
+  const container = document.createElement("div");
+  container.id = "result";
+  draw.appendChild(container);
+  const pTag = document.createElement("p");
+  container.appendChild(pTag);
+  const textValue = document.createTextNode(whatToDraw);
+  pTag.appendChild(textValue);
+};
 
 // Draw the array from the first implementation
 
 const arrayDrawer = () => {
-    eraser();
-    fibonacciFirst(fetcher());
-    let text = fibonacciFirstArray.toString().split(",").join(' ');
-    elementDrawer(text);
-}
+  eraser();
+  fibonacciFirst(fetcher());
+  let text = fibonacciFirstArray
+    .toString()
+    .split(",")
+    .join(" ");
+  elementDrawer(text);
+};
 
 // Draw on the screen
 
 const implementationDrawer = () => {
-    eraser();
-    let text = letMeCheck();
-    elementDrawer(text);
-}
+  eraser();
+  let text = letMeCheck();
+  elementDrawer(text);
+};
 
 // Function runner with performance time
 
 const runner = () => {
-    eraser()
-    let t0 = performance.now();
-    implementationDrawer();
-    let t1 = performance.now();
-    let timerResult = document.createElement('p');
-    timerResult.id = 'timer';
-    let timerResultText = `This function took ${t1 - t0} miliseconds.`;
-    let timerResultValue = document.createTextNode(timerResultText);
-    timerResult.appendChild(timerResultValue);
-    draw.appendChild(timerResult);
-}
+  eraser();
+  let t0 = performance.now();
+  implementationDrawer();
+  let t1 = performance.now();
+  let timerResult = document.createElement("p");
+  timerResult.id = "timer";
+  let timerResultText = `This function took ${t1 - t0} miliseconds.`;
+  let timerResultValue = document.createTextNode(timerResultText);
+  timerResult.appendChild(timerResultValue);
+  draw.appendChild(timerResult);
+};
 
 // Let's handle all the stuff!
 
 firstImplementation.addEventListener("click", () => {
-    clickHandlerForFirst();
-    runner();
+  clickHandlerForFirst();
+  runner();
 });
 
 secondImplementation.addEventListener("click", () => {
-    clickHandlerForSecond();
-    runner();
+  clickHandlerForSecond();
+  runner();
 });
 
 thirdImplementation.addEventListener("click", () => {
-    clickHandlerForThird();
-    runner();
+  clickHandlerForThird();
+  runner();
 });
 
 firstImplementationArray.addEventListener("click", () => {
-    arrayDrawer();
-})
+  arrayDrawer();
+});
--- pascal/pascal.js
+++ pascal/pascal.js
@@ -1,78 +1,82 @@
 // Variables
 
 const draw = document.getElementById("draw");
-const numberBox = document.getElementById('numberBox');
-const controls = document.getElementById('controlBox');
+const numberBox = document.getElementById("numberBox");
+const controls = document.getElementById("controlBox");
 
 // Functions
 
-const pascalTriangle = (rowsNum) => {
-    let pascalTriangle = [];
-    for (let i=0; i<rowsNum; i++) {
-        pascalTriangle[i] = new Array(i+1);
-        for (let j=0; j<i+1; j++) {
-            if (j === 0 || j === i) {
-                pascalTriangle[i][j] = 1;
-            } else {
-                pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
-            }
-        }
+const pascalTriangle = rowsNum => {
+  let pascalTriangle = [];
+  for (let i = 0; i < rowsNum; i++) {
+    pascalTriangle[i] = new Array(i + 1);
+    for (let j = 0; j < i + 1; j++) {
+      if (j === 0 || j === i) {
+        pascalTriangle[i][j] = 1;
+      } else {
+        pascalTriangle[i][j] =
+          pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+      }
     }
-    return pascalTriangle;
-}
+  }
+  return pascalTriangle;
+};
 
 // If I want to get the result in the table in the console
 
-const drawTable = (number) => {
-    console.table(pascalTriangle(number))
-}
+const drawTable = number => {
+  console.table(pascalTriangle(number));
+};
 
 // Fetch the value from input
 
 const fetcher = () => {
-    let numberBoxValue = numberBox.value;
-    return numberBoxValue;
-}
+  let numberBoxValue = numberBox.value;
+  return numberBoxValue;
+};
 
 // Erase from the screen
 
 const eraser = () => {
-    let childNode = document.getElementById('result');
-    let timer = document.getElementById('timer');
-    if (childNode !== null && timer !== null) {
-        draw.removeChild(childNode);
-        controlBox.removeChild(timer);
-    };
-}
+  let childNode = document.getElementById("result");
+  let timer = document.getElementById("timer");
+  if (childNode !== null && timer !== null) {
+    draw.removeChild(childNode);
+    controlBox.removeChild(timer);
+  }
+};
 
 // Draw result on the screen
 
 const drawer = () => {
-    eraser();
-    const result = pascalTriangle(fetcher());
-    const container = document.createElement('div');
-    container.id = 'result';
-    draw.appendChild(container);
-    for (let i=0; i<result.length; i++) {
-        let pTag = document.createElement('p');
-        container.appendChild(pTag);
-        pTag.id = `line${i}`;
-        let text = `${result[i]}`.toString().split(",").join(' ');
-        let textValue = document.createTextNode(text);
-        pTag.appendChild(textValue);
-    };
-}
+  eraser();
+  const result = pascalTriangle(fetcher());
+  const container = document.createElement("div");
+  container.id = "result";
+  draw.appendChild(container);
+  for (let i = 0; i < result.length; i++) {
+    let pTag = document.createElement("p");
+    container.appendChild(pTag);
+    pTag.id = `line${i}`;
+    let text = `${result[i]}`
+      .toString()
+      .split(",")
+      .join(" ");
+    let textValue = document.createTextNode(text);
+    pTag.appendChild(textValue);
+  }
+};
 
 // Function runner with performance time
 
 const runner = () => {
-    let t0 = performance.now();
-    drawer();
-    let t1 = performance.now();
-    let timerResult = document.createElement('p');
-    timerResult.id = 'timer';
-    let timerResultText = `This function took ${t1 - t0} miliseconds.`;
-    let timerResultValue = document.createTextNode(timerResultText);
-    timerResult.appendChild(timerResultValue);
-    controlBox.appendChild(timerResult);
-}
+  let t0 = performance.now();
+  drawer();
+  let t1 = performance.now();
+  let timerResult = document.createElement("p");
+  timerResult.id = "timer";
+  let timerResultText = `This function took ${t1 - t0} miliseconds.`;
+  let timerResultValue = document.createTextNode(timerResultText);
+  timerResult.appendChild(timerResultValue);
+  controlBox.appendChild(timerResult);
+};
