39d6c20ea0a9e0fd19ad270f7f4af4b7b1fe8ebf
arturkondas <artur.kondas@ecs-digital.co.uk>
2019-08-16 09:37:03 +0100

    Draft Go implementation

---
+++ rot13/go/rot13.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func rot13(inputString string) string {
+	input := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	output := "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
